d5b949b42a5200bbaadc5ec7c60fbc145eff9724
00nic <nicoenzogon@gmail.com>
2024-10-25 17:31:46 -0300

    logica para modal

--- server.py
+++ server.py
@@ -10,25 +10,29 @@ load_dotenv
 app.config['MYSQL_DB']= os.getenv('MYSQL_DB')
 app.config['MYSQL_HOST']= os.getenv('MYSQL_HOST')
 app.config['MYSQL_USER']= os.getenv('MYSQL_USER')
-app.config['MYSQL_PASSWORD']= os.getenv('MYSQL_HOST')
+app.config['MYSQL_PASSWORD']= os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_CURSORCLASS']= 'DictCursor'
 app.config['SECRET_KEY']= '4848'
 
 mysql = MySQL(app)
 
 
+@app.route('/portfolio', defaults= {'id_proyecto' : None})
 @app.route('/portfolio/<id_proyecto>')
-@app.route('/portfolio')
-def portfolio():
+def portfolio(id_proyecto):
     cur= mysql.connection.cursor()
     cur.execute('SELECT * FROM proyectos')
     proyectos= cur.fetchall()
+    proyecto = None
+    if id_proyecto is not None :
+        cur.execute('SELECT * FROM proyectos WHERE id_proyecto = %s', (id_proyecto,))
+        proyecto = cur.fetchone()
     cur.close()
-    return render_template('index.html', proyectos = proyectos)
+    return render_template('index.html', proyectos = proyectos, proyecto = proyecto)
 
 @app.route('/sobreMi')
 def sobreMi():
     return render_template('sobreMi.html')
 
-#if __name__ == '__main__':
-#    app.run(debug= True, port= 5000)
+if __name__ == '__main__':
+    app.run(debug= True, port= 5000)
