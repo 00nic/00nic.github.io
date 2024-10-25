c489e0d73dd0cd91d05ac9e74e6519355d00c4b1
00nic <nicoenzogon@gmail.com>
2024-10-25 17:34:00 -0300

    js del modal, img correspondiente para pestaña sobre mi, css para ambas pestañas

---
+++ static/js/modal.js
@@ -0,0 +1,6 @@
+var modal_proyecto = document.getElementById('modal-proyecto');
+window.onclick = function(event) {
+    if (event.target == modal_proyecto) {
+      window.location.href = '/portfolio';
+    }
+  }
